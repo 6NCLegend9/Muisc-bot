c8f47019d0ed163ca8ad34d61ac8f1f01abd28cd
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-01-04 16:35:03 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.TOKEN = 'NjYyOTg4NjUzMjk5MjM2ODc0.XhB-aQ.hqZA0XxytJsDVT-ytAy7qvoLJAs'; //THIS BOT IS FOR MULTIPLE GUILD
+exports.PREFIX = '!';
+
+exports.GOOGLE_API_KEY = 'AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w';
+exports.COLOR = '#fffff'; //COLOR OF EMBED
+exports.STATUS = `Music Bot By NC_Legend`; //ADD YOUR BOT STATUS
