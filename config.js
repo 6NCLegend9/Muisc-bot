22033ad23cf573074fc2656b2d9028824a8a7e43
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-06-01 15:01:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = 'NjI4MjE5MjEzOTk5Mzc0MzM3.XhCrSw.gBEi7kB98ejHlhkB8CJSZx0HQyQ'; 
+exports.TOKEN = ''; 
 exports.PREFIX = '-';
 
 exports.GOOGLE_API_KEY = 'AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w';
